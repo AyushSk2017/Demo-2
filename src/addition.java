2cbd0ca8bda78acfce82edad7b4daa6c8387da8a
AyushSk2017 <ayushsk2017@gmail.com>
2022-09-27 19:40:11 +0530

    first commit

---
+++ src/addition.java
@@ -0,0 +1,12 @@
+//Addition of two numbers
+//Author:Ayush Kadam
+//Date:21-07-22
+public class addition {
+    public static void main(String[]args)
+    {
+        int a=10,b=10;
+        int sum;
+        sum=a+b;
+        System.out.println("The addition:"+sum);
+    }
+}
